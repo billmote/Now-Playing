5d072d07917ea0bd151d2d55d9ee4c73ea6946c2
Martin Andersson <martin@phdlabs.com>
2014-09-08 10:59:12 -0500

    Cleaned up import statements

--- app/src/main/java/com/androidfu/foundation/ui/activities/MainActivity.java
+++ app/src/main/java/com/androidfu/foundation/ui/activities/MainActivity.java
@@ -1,41 +1,19 @@
 package com.androidfu.foundation.ui.activities;
 
-import android.app.DialogFragment;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.androidfu.foundation.R;
-import com.androidfu.foundation.model.ApplicationSettings;
 import com.androidfu.foundation.ui.fragments.PlaceholderFragment;
-import com.androidfu.foundation.ui.fragments.ReusableDialogFragment;
 import com.androidfu.foundation.util.EventBus;
 import com.androidfu.foundation.util.GoogleAnalyticsHelper;
 import com.androidfu.foundation.util.Log;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
-import com.squareup.otto.Subscribe;
 
 import butterknife.ButterKnife;
 import hugo.weaving.DebugLog;
-
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_APP_IS_DISABLED_FLAG;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_KILLSWITCH_MESSAGE;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_LAST_SEEN_MOTD_TIME_IN_MILLIS;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_MANDATORY_UPDATE_MESSAGE;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_MESSAGE_OF_THE_DAY;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_MOTD_LAST_UPDATED_TIME_IN_MILLIS;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_SHOW_UPDATE_NAG_FLAG;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_UPDATE_AVAILABLE_FLAG;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_UPDATE_NAG_MESSAGE;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.KEY_PREFS_UPDATE_REQUIRED_FLAG;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.getBoolean;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.getLong;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.getStringPreference;
-import static com.androidfu.foundation.util.SharedPreferencesHelper.putLong;
 
 
 public class MainActivity extends BaseActivity implements PlaceholderFragment.OnFragmentInteractionListener {
