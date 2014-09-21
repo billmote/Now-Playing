bd2de09790093a8770ead44fde92e9a540adea38
Bill Mote <bill@android-fu.com>
2014-09-21 13:33:17 -0400

    Updated fork script to easily identify what needs to be sought-and-replaced.

--- documentation/forkproject.sh
+++ documentation/forkproject.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-cd ~/git/shout/app
+# chmod +x ./forkproject.sh # make this file executable
+# Search and replace {NewRepoName}, {newappname} and {NewAppName}
+# NOTE: this will mess up 1 URL in res/values/build_properties.xml, but you'll want your own API endpoint anyway ;)
 
-mv ~/git/shout/app/src/main/java/com/androidfu/foundation ~/git/shout/app/src/main/java/com/androidfu/shout
+cd ~/git/{NewRepoName}/app
 
-grep -IRlr com.androidfu.foundation . | xargs sed -i "" -e 's/com.androidfu.foundation/com.androidfu.shout/g'
+mv ~/git/{NewRepoName}/app/src/main/java/com/androidfu/foundation ~/git/{NewRepoName}/app/src/main/java/com/androidfu/{newappname}
 
-grep -IRlr foundation . | xargs sed -i "" -e 's/foundation/shout/g'
+grep -IRlr com\.androidfu\.foundation . | xargs sed -i "" -e 's/com\.androidfu\.foundation/com\.androidfu\.{newappname}/g'
 
-grep -IRlr Foundation . | xargs sed -i "" -e 's/Foundation/Shout/g'
+grep -IRlr foundation . | xargs sed -i "" -e 's/foundation/{newappname}/g'
 
-mv ~/git/shout/app/src/main/java/com/androidfu/shout/FoundationApplication.java ~/git/shout/app/src/main/java/com/androidfu/shout/ShoutApplication.java
-mv ~/git/shout/Foundation.iml ~/git/shout/Shout.iml
+grep -IRlr Foundation . | xargs sed -i "" -e 's/Foundation/{NewAppName}/g'
+
+mv ~/git/{NewRepoName}/app/src/main/java/com/androidfu/{newappname}/FoundationApplication.java ~/git/{NewRepoName}/app/src/main/java/com/androidfu/{newappname}/{NewAppName}Application.java
+mv ~/git/{NewRepoName}/Foundation.iml ~/git/{NewRepoName}/{NewAppName}.iml
