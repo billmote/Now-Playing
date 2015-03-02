6a4280317eb46510b23da6caee8b27ef62c62c00
Bill Mote <bill.mote@gmail.com>
2015-03-01 22:05:36 -0800

    Working on RottenTomatoes API bug

--- app/src/main/java/com/androidfu/nowplaying/app/api/RestClient.java
+++ app/src/main/java/com/androidfu/nowplaying/app/api/RestClient.java
@@ -18,33 +18,24 @@ public class RestClient {
 
     public RestClient(String endpoint, boolean enableLogging) {
 
-        GsonConverter jsonParserConverter = new GsonConverter(buildJsonParser(), "UTF-8");
-        
         Gson gson = new GsonBuilder()
                 .registerTypeAdapterFactory(new ItemTypeAdapterFactory())
                 .setDateFormat("yyyy-MM-dd")
                 .setPrettyPrinting()
                 .excludeFieldsWithoutExposeAnnotation()
+                .excludeFieldsWithModifiers()
                 .create();
 
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setLogLevel(BuildConfig.DEBUG && enableLogging ? RestAdapter.LogLevel.FULL : RestAdapter.LogLevel.NONE)
                 .setEndpoint(endpoint)
-                .setConverter(jsonParserConverter)
-                //.setConverter(new GsonConverter(gson))
+                .setConverter(new GsonConverter(gson, "UTF-8"))
                 .setRequestInterceptor(new SessionRequestInterceptor())
                 .build();
 
         apiService = restAdapter.create(ApiService.class);
     }
 
-    public static Gson buildJsonParser() {
-        GsonBuilder jsonParserBuilder = new GsonBuilder();
-        jsonParserBuilder.setPrettyPrinting();
-        jsonParserBuilder.excludeFieldsWithoutExposeAnnotation();
-        return jsonParserBuilder.create();
-    }
-    
     public ApiService getApiService() {
         return apiService;
     }
