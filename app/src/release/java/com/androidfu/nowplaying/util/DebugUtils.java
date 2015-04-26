cc156b236a38ea46358b786c0682e9948df24e04
Bill Mote <bill@android-fu.com>
2015-04-26 13:26:39 -0400

    Fixed a crash related to Parceler.  Shipped version 5.

--- app/src/release/java/com/androidfu/nowplaying/util/DebugUtils.java
+++ app/src/release/java/com/androidfu/nowplaying/util/DebugUtils.java
@@ -5,16 +5,11 @@ import android.app.Activity;
 import hugo.weaving.DebugLog;
 
 /**
- * Add this to your application's src/debug/... sources
+ * @see {@link DebugUtils}
  */
 @DebugLog
 public class DebugUtils {
 
-    /**
-     * Show the activity over the lockscreen and wake up the device. If you launched the app manually
-     * both of these conditions are already true. If you deployed from the IDE, however, this will
-     * save you from hundreds of power button presses and pattern swiping per day!
-     */
     public static void riseAndShine(Activity activity) {
     }
 
